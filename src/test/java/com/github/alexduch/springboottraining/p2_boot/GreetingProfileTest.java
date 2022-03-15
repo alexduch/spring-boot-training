af1f77daec77be2abb7ddf9bd17116c73b0fc1b9
Alexandre Duchenne <alexandre.duchenne@lacombedulionvert.fr>
2022-03-15 14:04:36 +0100

    Ajouter un test de profils avec les fichiers de configuration

--- src/test/java/com/github/alexduch/springboottraining/p2_boot/GreetingProfileTest.java
+++ src/test/java/com/github/alexduch/springboottraining/p2_boot/GreetingProfileTest.java
@@ -3,9 +3,11 @@ package com.github.alexduch.springboottraining.p2_boot;
 import com.github.alexduch.springboottraining.core.Greeter;
 import com.github.alexduch.springboottraining.core.GreetingProvider;
 import com.github.alexduch.springboottraining.core.Hello;
+import com.github.alexduch.springboottraining.extensions.config.GreeterConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("6 Utiliser les profils Spring Boot")
 class GreetingProfileTest {
 
-  @ParameterizedTest(name = "Avec le profil {0}")
+  @DisplayName("Avec l'annotation @Profile")
+  @ParameterizedTest(name = "En activant le profil {0}")
   @CsvSource(
       value = {"test|Hey, this is a test!", "prod|Hello world!"},
       delimiter = '|')
@@ -24,6 +27,24 @@ class GreetingProfileTest {
     new ApplicationContextRunner()
         .withUserConfiguration(GreetingProfileConfiguration.class)
         .withPropertyValues("spring.profiles.active=" + profile)
+        .run(
+            context ->
+                assertThat(context)
+                    .getBean(Greeter.class)
+                    .extracting(greeter -> greeter.greet("world"))
+                    .isEqualTo(expectedGreeting));
+  }
+
+  @DisplayName("Avec des fichiers de configuration")
+  @ParameterizedTest(name = "En activant le profil {0}")
+  @CsvSource(
+      value = {"test|Hey, this is a test!", "prod|Hello world!"},
+      delimiter = '|')
+  void testGreetingProfileSelectionWithConfiguration(String profile, String expectedGreeting) {
+    new ApplicationContextRunner()
+        .withUserConfiguration(GreeterConfiguration.class)
+        .withPropertyValues("spring.profiles.active=" + profile)
+        .withInitializer(new ConfigDataApplicationContextInitializer())
         .run(
             context ->
                 assertThat(context)
